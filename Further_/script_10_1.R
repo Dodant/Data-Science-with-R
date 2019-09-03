0092534336013e4fc4494adb4ced0a2359b55571
Dodanto <ohho0728@naver.com>
2019-09-04 00:37:26 +0900

    Problem SOLVED

--- Further_/script_10_1.R
+++ Further_/script_10_1.R
@@ -1,12 +1,11 @@
-dyn.load('/Library/Java/JavaVirtualMachines/openjdk-12.0.1.jdk/Contents/Home/lib/server/libjvm.dylib')
-
-install.packages("rJava")
-install.packages("memoise")
-install.packages("KoNLP")
-
+library(rJava)
 library(KoNLP)
 library(dplyr)
 
 useNIADic()
 
-Sys.getenv("JAVA_HOME")
+txt <- readLines(file("hiphop.txt", encoding = "EUC-KR"))
+head(txt)
+
+install.packages("stringr")
+library(stringr)
