b5b1ccd066175e98d1194ced24a2021703e6f246
Dodanto <ohho0728@naver.com>
2019-09-04 21:31:10 +0900

    Furt_10 Done

--- Further_/script_10_1.R
+++ Further_/script_10_1.R
@@ -1,11 +1,53 @@
 library(rJava)
 library(KoNLP)
 library(dplyr)
+library(stringr)
 
 useNIADic()
 
 txt <- readLines(file("hiphop.txt", encoding = "EUC-KR"))
 head(txt)
 
-install.packages("stringr")
-library(stringr)
+txt <- str_replace_all(txt, "\\W", " ")
+
+extractNoun("대한민국의 영토는 한반도와 그 부속도서로 한다.")
+
+nouns <- extractNoun(txt)
+wordcount <- table(unlist(nouns))
+df_word <- as.data.frame(wordcount, stringsAsFactors = F)
+df_word <- rename(df_word,
+                  word = Var1,
+                  freq = Freq)
+
+df_word <- filter(df_word, nchar(word) >= 2)
+top20 <- df_word %>% 
+  arrange(desc(freq)) %>% 
+  head(20)
+top20
+
+library(wordcloud)
+library(RColorBrewer)
+
+pal <- brewer.pal(8, "Dark2")
+set.seed(1234)
+
+wordcloud(words = df_word$word,
+          freq = df_word$freq,
+          min.freq = 2,
+          max.words = 200,
+          random.order = F,
+          rot.per = .1,
+          scale = c(4, 0.3),
+          colors = pal)
+
+pal <- brewer.pal(9, "Blues")[5:9]
+set.seed(1234)
+
+wordcloud(words = df_word$word,
+          freq = df_word$freq,
+          min.freq = 2,
+          max.words = 200,
+          random.order = F,
+          rot.per = .1,
+          scale = c(4, 0.3),
+          colors = pal)
